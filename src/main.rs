49f3ab0e9af5991b8ad33007aec4306a23a7a151
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2024-12-23 06:06:14 +0100

    arrays in rust

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#[allow(unused_variables)]
+#[allow(unused)]
 fn read_version(transaction_hex: &str) -> u32 {
-    return 1;
+    let tx_bytes = hex::decode(transaction_hex).unwrap();
+    let version_bytes = <[u8; 4]>::try_from(&tx_bytes[0..4]).unwrap();
+    u32::from_le_bytes(version_bytes)
 }
 
 #[allow(unused_variables)]
 fn main() {
-    let version = read_version("	4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5");
+    let version = read_version("0100000000001c47896df6c74aa8351f371feef54d0b9a90516d74ebe4d0d828");
     println!("version: {}", version);
 }
 
